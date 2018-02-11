881ff14a417505c7b1836c77b917c16d0a5cf070
txgz <975663670@qq.com>
2018-02-11 17:21:14 +0800

    12

--- ttsx/ttsx/settings.py
+++ ttsx/ttsx/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'index',
     'userInfo',
+    'ttsx_goods',
 )
 
 MIDDLEWARE_CLASSES = (
--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^',include('index.urls')),
     url(r'^user/',include('userInfo.urls')),
+    url(r'^',include('ttsx_goods.urls')),
+
 ]
